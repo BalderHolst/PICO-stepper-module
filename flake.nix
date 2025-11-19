3be06c4747d9df7d639dbac30542a5d13a8b747d
BalderHolst <balderwh@gmail.com>
2025-11-19 23:25:57 +0100

    Clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flake utils demo";
+  description = "Flake for MicroPython development on Raspberry Pi Pico";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -12,14 +12,11 @@
         {
             devShells.default = pkgs.mkShell {
                 buildInputs = with pkgs; [
-                    mpremote              # MicroPython CLI remote control tool
-                    thonny                # IDE for MicroPython
-                    minicom               # Serial monitor
-                    picotool              # Raspberry Pi Pico utility
-                    cmake                 # Build system
-                    clang-tools           # C/C++ tools
-                    gcc-arm-embedded-14   # ARM Embedded GCC toolchain
-                    pkgsi686Linux.gcc     # 32-bit GCC toolchain
+                    mpremote           # MicroPython CLI remote control tool
+                    minicom            # Serial monitor
+                    picotool           # Raspberry Pi Pico utility
+                    cmake              # Build system
+                    gcc-arm-embedded   # ARM Embedded GCC toolchain
                 ];
             };
         }
